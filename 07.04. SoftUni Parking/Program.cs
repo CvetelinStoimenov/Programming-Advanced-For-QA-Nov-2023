58bf725c8c1945fab6b4e05e7a7d26a87e71c80b
Cvetelin Stoimenov <45821593+Mishenata@users.noreply.github.com>
2023-11-16 13:08:52 +0200

    new

---
+++ 07.04. SoftUni Parking/Program.cs
@@ -0,0 +1,96 @@
+﻿/*
+ 4.	SoftUni Parking
+SoftUni just got a new parking lot. It's so fancy, it even has online parking validation. Except the online service doesn't work. It can only receive users' data, but it doesn't know what to do with it. Good thing you're on the dev team and know how to fix it, right?
+Write a program, which validates a parking place for an online service. Users can register to park and unregister to leave.
+The program receives 2 commands:	
+•	"register {username} {licensePlateNumber}":
+o	The system only supports one car per user at the moment, so if a user tries to register another license plate, using the same username, the system should print:
+"ERROR: already registered with plate number {licensePlateNumber}"
+o	If the aforementioned checks passes successfully, the plate can be registered, so the system should print:
+ "{username} registered {licensePlateNumber} successfully"
+•	"unregister {username}":
+o	If the user is not present in the database, the system should print:
+"ERROR: user {username} not found"
+o	If the aforementioned check passes successfully, the system should print:
+"{username} unregistered successfully"
+After you execute all of the commands, print all of the currently registered users and their license plates in the format: 
+•	"{username} => {licensePlateNumber}"
+Input
+•	First line: n - number of commands – integer.
+•	Next n lines: commands in one of the two possible formats:
+o	Register: "register {username} {licensePlateNumber}"
+o	Unregister: "unregister {username}"
+The input will always be valid and you do not need to check it explicitly.
+Examples
+Input	                    Output
+5
+register John CS1234JS      John registered CS1234JS successfully
+register George JAVA123S    George registered JAVA123S successfully
+register Andy AB4142CD      Andy registered AB4142CD successfully
+register Jesica VR1223EE    Jesica registered VR1223EE successfully
+unregister Andy             Andy unregistered successfully
+                            John => CS1234JS
+                            George => JAVA123S
+                            Jesica => VR1223EE
+
+Input	                    Output
+4                           Jony registered AA4132BB successfully
+register Jony AA4132BB      ERROR: already registered with plate number AA4132BB
+register Jony AA4132BB      Linda registered AA9999BB successfully
+register Linda AA9999BB     Jony unregistered successfully
+unregister Jony             Linda => AA9999BB
+
+Input	                    Output
+6                           Jacob registered MM1111XX successfully
+        Anthony registered AB1111XX successfully
+        Jacob unregistered successfully
+        Joshua registered DD1111XX successfully
+        ERROR: user Lily not found
+        Samantha registered AA9999BB successfully
+        Joshua => DD1111XX
+        Anthony => AB1111XX
+        Samantha => AA9999BB*/
+
+Dictionary<string, string> registrars = new Dictionary<string, string>();
+
+int n = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    string[] commandArray = Console.ReadLine().Split().ToArray();
+
+    string command = commandArray[0];
+    string employee = commandArray[1];
+
+    if (command == "register")
+    {
+        string plateNumber = commandArray[2];
+
+        if (!registrars.ContainsKey(employee))
+        {
+            registrars.Add(employee, plateNumber);
+            Console.WriteLine($"{employee} registered {plateNumber} successfully");
+        }
+        else
+        {
+            Console.WriteLine($"ERROR: already registered with plate number {registrars[employee]}");
+        }
+    }
+    else if (command == "unregister")
+    {
+        if (registrars.ContainsKey(employee))
+        {
+            registrars.Remove(employee);
+            Console.WriteLine($"{employee} unregistered successfully");
+        }
+        else
+        {
+            Console.WriteLine($"ERROR: user {employee} not found");
+        }
+    }
+}
+
+foreach (KeyValuePair<string, string> pair in registrars)
+{
+    Console.WriteLine($"{pair.Key} => {pair.Value}");
+}
---
+++ 07.06. Company Users/Program.cs
@@ -0,0 +1,68 @@
+﻿/*
+ 6.	*Company Users
+Create a program that keeps information about companies and their employees. 
+You will be receiving a company name and an employee's id, until you receive the "End" command. Add each employee to the given company. Keep in mind that a company cannot have two employees with the same id.
+When you finish reading the data, print the company's name and each employee's id in the following format:
+{companyName}
+-- {id1}
+-- {id2}
+-- {idN}
+Input / Constraints
+    •	Until you receive the "End" command, you will be receiving input in the format: "{companyName} -> {employeeId}".
+    •	The input always will be valid.
+
+Examples
+
+Input	                    Output
+SoftUni -> AA12345          SoftUni
+SoftUni -> BB12345          -- AA12345
+Microsoft -> CC12345        -- BB12345
+HP -> BB12345               Microsoft
+End                         -- CC12345
+                            HP
+                            -- BB12345
+
+
+Input	                    Output
+SoftUni -> AA12345          SoftUni
+SoftUni -> CC12344          -- AA12345
+Lenovo -> XX23456           -- CC12344
+SoftUni -> AA12345          Lenovo
+Movement -> DD11111         -- XX23456
+End                         Movement
+                            -- DD11111*/
+
+Dictionary<string, List<string>> companiees = new Dictionary<string, List<string>>();
+
+string input = Console.ReadLine();
+
+while (input != "End")
+{
+    string[] inputArray = input.Split(" -> ");
+    string company = inputArray[0];
+    string employeeID = inputArray[1];
+
+    if (!companiees.ContainsKey(company))
+    { 
+    
+    companiees.Add(company, new List<string>());
+    }
+
+    if (!companiees[company].Contains(employeeID))
+    {
+        companiees[company].Add(employeeID);
+    }
+
+    input = Console.ReadLine();
+
+}
+
+foreach (KeyValuePair<string, List<string>> currentCompany in companiees)
+{
+    Console.WriteLine(currentCompany.Key);
+
+    foreach (string currentEmployeeID in currentCompany.Value)
+    {
+        Console.WriteLine($"-- {currentEmployeeID}");
+    }
+}
