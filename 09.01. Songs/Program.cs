97b77ee7c66ea5d49a02ca5e8e59ca14df265416
Cvetelin Stoimenov <45821593+Mishenata@users.noreply.github.com>
2023-11-25 19:37:47 +0200

    Add

--- 09.01. Songs/Program.cs
+++ 09.01. Songs/Program.cs
@@ -8,6 +8,7 @@ namespace _09._01._Songs
         static void Main(string[] args)
         {
             int numberOfSongs = int.Parse(Console.ReadLine());
+            List<Song> songs = new List<Song>();
 
             for (int i = 0; i < numberOfSongs; i++)
             {
@@ -16,6 +17,45 @@ namespace _09._01._Songs
                 string songName = songComponents[1];
                 string songTime = songComponents[2];
 
+                Song currentSong = new Song
+                {
+                    Name = songName,
+                    TypeList = songTypeList,
+                    Time = songTime
+                };
+
+                songs.Add(currentSong);
+
+            }
+
+            string typeCommand = Console.ReadLine();
+
+            if (typeCommand != "all")
+            {
+                songs.Where(x => x.TypeList == typeCommand).ToList();
+            }
+
+            foreach(Song song in songs)
+            {
+                Console.WriteLine(song.Name);
+            }
+            //if (typeCommand == "all")
+            //{
+            //    foreach (Song song in songs)
+            //    {
+            //        Console.WriteLine(song.Name);
+            //    }
+            //}
+            //else 
+            //{
+            //    List<Song> filteredSongs = songs
+            //        .Where(x => x.TypeList == typeCommand)
+            //        .ToList();
+
+            //    foreach (Song song in filteredSongs)
+            //    {
+            //        Console.WriteLine(song.Name);
+            //    }
             }
         }
     }
--- 09.03. Store Boxes/Program.cs
+++ 09.03. Store Boxes/Program.cs
@@ -1,10 +1,1 @@
-﻿namespace _09._03._Store_Boxes
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            
-        }
-    }
-}
+﻿s
