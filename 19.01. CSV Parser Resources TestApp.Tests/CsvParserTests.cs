db27c0945e3e7d0eb7e889c0406e1080f426163c
Cvetelin Stoimenov <45821593+Mishenata@users.noreply.github.com>
2023-12-12 20:55:08 +0200

    12.12.2023

---
+++ 19.01. CSV Parser Resources TestApp.Tests/CsvParserTests.cs
@@ -0,0 +1,31 @@
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+[TestFixture]
+public class CsvParserTests
+{
+    [Test]
+    public void Test_ParseCsv_EmptyInput_ReturnsEmptyArray()
+    {
+        // TODO: finish the test
+    }
+
+    [Test]
+    public void Test_ParseCsv_SingleField_ReturnsArrayWithOneElement()
+    {
+        // TODO: finish the test
+    }
+
+    [Test]
+    public void Test_ParseCsv_MultipleFields_ReturnsArrayWithMultipleElements()
+    {
+        // TODO: finish the test
+    }
+
+    [Test]
+    public void Test_ParseCsv_TrimsWhiteSpace_ReturnsCleanArray()
+    {
+        // TODO: finish the test
+    }
+}
